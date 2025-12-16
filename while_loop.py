191c174b4f7479af4598de42cabc606f6009c347
KING <matadeenyadav202@gmail.com>
2025-12-16 12:48:17 +0530

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,62 @@
+# a = int(input("please tell your number"))
+
+# while a > 0:
+#     print(a % 10)
+#     a = a //10
+
+# a = int(input("please tell your number"))
+# s = 0 
+
+# while a > 0:
+#     s = s + a %10
+#     a = a //10
+
+# print(f"Your digits sum is {s}")
+
+
+# a = int(input("please tell your number"))
+
+# rev = 0 
+
+# while a > 0:
+#     rev = rev * 10 + a%10
+#     a = a //10
+
+# print(f"your number reverse is {rev}")
+
+
+
+
+# a = int(input("please tell your number"))
+# copy = a
+# rev = 0 
+
+# while a > 0:
+#     rev = rev * 10 + a%10
+#     a = a //10
+
+# if rev == copy:
+#     print("yes your number is pallindrome")
+# else:
+#     print("sorry your number is not pallindrome")
+
+
+
+# a = 6
+# dup = a
+# square = a ** 2 
+
+# count = 0
+
+# while a > 0:
+#     count = count + 1 
+#     a = a //10 
+
+# extract = square % (10**count)
+
+# if extract == dup:
+#     print("your number is automorphic")
+
+# else:
+#     print("sorry not an automorphic number")
+
